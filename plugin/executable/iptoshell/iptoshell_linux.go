d070b74e287a479ead2e6b4e9377283e393e849d
mark-syctnet <81214737+mark-syctnet@users.noreply.github.com>
2023-12-12 18:00:37 +0800

    Update iptoshell_linux.go

--- plugin/executable/iptoshell/iptoshell_linux.go
+++ plugin/executable/iptoshell/iptoshell_linux.go
@@ -80,10 +80,8 @@ func (p *iptoshellPlugin) addIPtoshell(r *dns.Msg) error {
                         prefix, okprefix := addr.Prefix(p.args.Mask4)
                         if okprefix != nil {
                                 return fmt.Errorf("iptoshell to Prefix  invalid A record with ip: %s", rr.A)
-                        }
-                        argshells := strings.Fields(p.args.SetName4)
-                        
-                        cmd := exec.Command(argshells[0], rr.A.String(), strconv.Itoa(p.args.Mask4), prefix.String(), argshells[1:]... )
+                        }  
+                        cmd := exec.Command(p.args.SetName4, rr.A.String(), strconv.Itoa(p.args.Mask4), prefix.String())
                         err := cmd.Run()
                         if err != nil {
                               return fmt.Errorf(" RUN iptoshell invalid  A record with ip: %s", rr.A)
@@ -103,8 +101,7 @@ func (p *iptoshellPlugin) addIPtoshell(r *dns.Msg) error {
                         if okprefix != nil {
                                 return fmt.Errorf("iptoshell to Prefix  invalid AAAA record with ip: %s", rr.AAAA)
                         }
-                        argshells := strings.Fields(p.args.SetName6)
-                        cmd := exec.Command(argshells[0], rr.AAAA.String(), strconv.Itoa(p.args.Mask6), prefix.String(), argshells[1:]...)
+                        cmd := exec.Command(p.args.SetName6, rr.AAAA.String(), strconv.Itoa(p.args.Mask6), prefix.String())
                         err := cmd.Run()
                         if err != nil {
                              return fmt.Errorf("Run iptoshell AAAA record with ip: %s", rr.AAAA)
