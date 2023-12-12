5864b366db8478a086ed15e7a57382f9f50fba7d
mark-syctnet <81214737+mark-syctnet@users.noreply.github.com>
2023-12-12 17:24:15 +0800

    Update iptoshell.go

--- plugin/iptoshell/iptoshell.go
+++ plugin/iptoshell/iptoshell.go
@@ -20,22 +20,56 @@
 package iptoshell
 
 import (
-	"github.com/IrineSistiana/mosdns/v5/coremain"
+        "fmt"
+        "github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
+        "strconv"
+        "strings"
 )
 
 const PluginType = "iptoshell"
 
 func init() {
-	coremain.RegNewPluginFunc(PluginType, Init, func() interface{} { return new(Args) })
+        sequence.MustRegExecQuickSetup(PluginType, QuickSetup)
 }
 
 type Args struct {
-	SetName4 string `yaml:"set_bash_name4"`
-	SetName6 string `yaml:"set_bash_name6"`
-	Mask4    int    `yaml:"mask4"` // default 24
-	Mask6    int    `yaml:"mask6"` // default 32
+        SetName4 string `yaml:"set_bash_name4"`
+        SetName6 string `yaml:"set_bash_name6"`
+        Mask4    int    `yaml:"mask4"` // default 24
+        Mask6    int    `yaml:"mask6"` // default 32
 }
 
-func Init(bp *coremain.BP, args interface{}) (p coremain.Plugin, err error) {
-	return newiptoshellPlugin(bp, args.(*Args))
+var _ sequence.Executable = (*iptoshellPlugin)(nil)
+
+// QuickSetup format: [set_name,{inet|inet6},mask] *2
+// e.g. "my_set,inet,24 my_set6,inet6,48"
+func QuickSetup(_ sequence.BQ, s string) (any, error) {
+        fs := strings.Fields(s)
+        if len(fs) > 2 {
+                return nil, fmt.Errorf("iptoshell expect no more than 2 fields, got %d", len(fs))
+        }
+
+        args := new(Args)
+        for _, argsStr := range fs {
+                ss := strings.Split(argsStr, ",")
+                if len(ss) != 3 {
+                        return nil, fmt.Errorf("iptoshell invalid args, expect 5 fields, got %d", len(ss))
+                }
+
+                m, err := strconv.Atoi(ss[2])
+                if err != nil {
+                        return nil, fmt.Errorf("iptoshell invalid mask, %w", err)
+                }
+                switch ss[1] {
+                case "inet":
+                        args.Mask4 = m
+                        args.SetName4 = ss[0]
+                case "inet6":
+                        args.Mask6 = m
+                        args.SetName6 = ss[0]
+                default:
+                        return nil, fmt.Errorf("iptoshell invalid set family, %s", ss[0])
+                }
+        }
+        return newIpToshellPlugin(args)
 }
