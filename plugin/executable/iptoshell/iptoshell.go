f97f3d11a0b89a5c0745863826744deef63cccad
mark-syctnet <81214737+mark-syctnet@users.noreply.github.com>
2023-12-14 12:56:08 +0800

    Update iptoshell.go

--- plugin/executable/iptoshell/iptoshell.go
+++ plugin/executable/iptoshell/iptoshell.go
@@ -37,11 +37,12 @@ type Args struct {
         SetName6 string `yaml:"set_bash_name6"`
         Mask4    int    `yaml:"mask4"` // default 24
         Mask6    int    `yaml:"mask6"` // default 32
+        Tagnum   int32    `yaml:"tagnum"` // default 0 
 }
 
 var _ sequence.Executable = (*iptoshellPlugin)(nil)
 
-// QuickSetup format: [set_name,{inet|inet6},mask] *2
+// QuickSetup format: [set_name,{inet|inet6},mask,tagnum] *2
 // e.g. "my_set,inet,24 my_set6,inet6,48"
 func QuickSetup(_ sequence.BQ, s string) (any, error) {
         fs := strings.Fields(s)
@@ -52,21 +53,27 @@ func QuickSetup(_ sequence.BQ, s string) (any, error) {
         args := new(Args)
         for _, argsStr := range fs {
                 ss := strings.Split(argsStr, ",")
-                if len(ss) != 3 {
-                        return nil, fmt.Errorf("iptoshell invalid args, expect 5 fields, got %d", len(ss))
+                if len(ss) != 4 {
+                        return nil, fmt.Errorf("iptoshell invalid args, expect 6 fields, got %d", len(ss))
                 }
 
                 m, err := strconv.Atoi(ss[2])
                 if err != nil {
                         return nil, fmt.Errorf("iptoshell invalid mask, %w", err)
                 }
+                tagnum, err2 := strconv.ParseInt(ss[3], 10, 32)
+                if err2 != nil {
+                        return nil, fmt.Errorf("iptoshell invalid tagnum, %w", err2)
+                }
                 switch ss[1] {
                 case "inet":
                         args.Mask4 = m
                         args.SetName4 = ss[0]
+                        args.Tagnum = tagnum
                 case "inet6":
                         args.Mask6 = m
                         args.SetName6 = ss[0]
+                        args.Tagnum = tagnum
                 default:
                         return nil, fmt.Errorf("iptoshell invalid set family, %s", ss[0])
                 }
